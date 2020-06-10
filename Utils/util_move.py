e95da5c87ba400f390f31aa35cf0439f3332065e
Moses Bomera <mosesbomer@gmail.com>
2020-06-10 22:49:50 +0300

    Add file mover tool.

---
+++ Utils/util_move.py
@@ -0,0 +1,31 @@
+# This script moves files from a specified directory to another directory.
+import os
+from sys import argv, exit
+
+# Get current directory
+directory = os.getcwd()
+# print(os.listdir('.'))
+
+if len(argv) != 2:
+    print("Usage: util.py file.txt")
+    exit(1)
+
+# os.rename(os.path.join(directory, 'dataset', 'train.txt'), os.path.join(directory, 'train.txt'))
+file = os.path.join(directory, argv[1])
+
+with open(file, 'r')  as train:
+    lines = train.readlines()
+
+    for line in lines:
+        # Get details
+        contents = line.split('/')
+
+        # the exact name of the file
+        filename = contents[-1].strip('\n')
+
+        type = argv[1].split('.')[0]
+        # new
+        new_dir = os.path.join(directory, 'dataset', type, filename)
+
+        # print(type, new_dir)
+        os.rename(line.rstrip('\n'), new_dir)
